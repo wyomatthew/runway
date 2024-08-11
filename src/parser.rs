b4b1af7aad9c30344c0c4f97137ba4bd81e13469
Matthew Pickering <wyomatthew@hotmail.com>
2024-08-11 13:29:30 -0500

    feat: adding parse stubs for all syntax tree types
    Signed-off-by: Matthew Pickering <wyomatthew@hotmail.com>

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,5 @@
+use std::path::Prefix;
+
 use nom::{
     branch::alt,
     bytes::complete::tag,
@@ -9,6 +11,12 @@ use nom::{
     IResult,
 };
 
+use crate::syntax_tree::{
+    AliasExpression, AssignmentExpression, BinaryOperator, Config, DeclarationExpression,
+    EvalExpression, Function, Identifier, LocField, Operation, OperatorExpression, Query,
+    SortOrder, SourceStatement, SourceType, Stage, TagList, TopQuantifier, UnaryOperator,
+};
+
 mod literal;
 
 /// Represents a type that can be parsed from a string
@@ -16,4 +24,118 @@ pub trait Parsable: Sized {
     /// Parses an instance of the calling type from a string. Returns the
     /// unparsed remainder of the input string and the parsed instance.
     fn parse(input: &str) -> IResult<&str, Self>;
+}
+
+impl Parsable for Query {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
+}
+
+impl Parsable for Config {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
+}
+
+impl Parsable for SourceStatement {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
+}
+
+impl Parsable for SourceType {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
+}
+
+impl Parsable for Operation {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
+}
+
+impl Parsable for Stage {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
+}
+
+impl Parsable for LocField {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
+}
+
+impl Parsable for TopQuantifier {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
+}
+
+impl Parsable for EvalExpression {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
+}
+
+impl Parsable for DeclarationExpression {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
+}
+
+impl Parsable for AssignmentExpression {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
+}
+
+impl Parsable for Function {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
+}
+
+impl Parsable for AliasExpression {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
+}
+
+impl Parsable for Identifier {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
+}
+
+impl Parsable for SortOrder {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
+}
+
+impl Parsable for OperatorExpression {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
+}
+
+impl Parsable for BinaryOperator {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
+}
+
+impl Parsable for UnaryOperator {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
+}
+
+impl Parsable for TagList {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        todo!()
+    }
 }
